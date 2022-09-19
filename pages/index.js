56a47cb0e57677bba3eba313b9f166a2487fcb0d
Heemo Yang <heemo521@gmail.com>
2022-09-18 18:45:47 -0700

    Init

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+function HomePage() {
+  return (
+    <div>
+      <h1>The Home Page</h1>
+      <p>My name is Heemo</p>
+    </div>
+  );
+}
+HomePage.propTypes = {};
+export default HomePage;
---
+++ pages/news.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NewsPage(props) {
+  return <div>NewsPage</div>;
+}
+
+NewsPage.propTypes = {};
+
+export default NewsPage;
