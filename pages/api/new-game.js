91ab408fcdb76c93fb5334c8a1cc1f5e53323b99
Heemo Yang <heemo521@gmail.com>
2022-09-22 01:37:42 -0700

    Create cloud db and connect from api

--- pages/api/new-game.js
+++ pages/api/new-game.js
@@ -1,10 +1,18 @@
+import { MongoClient } from 'mongodb';
+import { uri } from '../../components/tempENV';
+
 // API for creating a new game
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
     // need the game creator's id
     const { gameMaster } = data;
+
+    const client = await MongoClient.connect(uri);
+    const db = client.db();
+
+    const gameCollection = db.collection('secret_hitler');
   }
 }
