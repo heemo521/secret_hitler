b0e47907cbdc0dc756efc529dd111414c347b7e5
Heemo Yang <heemo521@gmail.com>
2022-09-22 01:10:38 -0700

    Build test api for new game

--- pages/api/new-game.js
+++ pages/api/new-game.js
@@ -1,3 +1,10 @@
 // API for creating a new game
 
-function handler() {}
+function handler(req, res) {
+  if (req.method === 'POST') {
+    const data = req.body;
+
+    // need the game creator's id
+    const { gameMaster } = data;
+  }
+}
