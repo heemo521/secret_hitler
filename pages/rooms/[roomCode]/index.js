5d1967920a4927022f5203739ea239773e54faa9
Heemo Yang <heemo521@gmail.com>
2022-09-22 00:07:15 -0700

    Refactor game room and lobby using getstaticprops

--- pages/rooms/[roomCode]/index.js
+++ pages/rooms/[roomCode]/index.js
@@ -1,44 +1,53 @@
-import React, { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import React, { useState } from 'react';
 import WaitingRoom from '../../../components/rooms/WaitingRoom';
 import GameBoard from '../../../components/game/GameBoard';
 import PropTypes from 'prop-types';
 
-function Game() {
-  const router = useRouter();
+function Game({ roomCode, isRoomCodeValid }) {
   const [startGame, setStartGame] = useState(false);
-  const [roomCodeValidity, setRoomCodeValidity] = useState(false);
-  const { roomCode } = router.query;
 
   // use localstorage for now to save user data??
 
-  useEffect(() => {
-    // Make some query to the server to see if the room exists or if the game is already started etc.
-    // should I allow spectator mode? Hmm...
-    verifyRoomCode(roomCode);
-  }, [roomCode]);
-
-  function verifyRoomCode(roomCode) {
-    //make sure that this room exists and this player is registered
-    // if not registered then spectator?
-    // axios(roomCode)
-    console.log(roomCode);
-    setRoomCodeValidity(true);
-  }
-
-  if (!roomCode || !roomCodeValidity) {
+  if (!roomCode || !isRoomCodeValid) {
     return <h1>this room does not exist fool!!!</h1>;
   }
 
   if (!startGame) {
     return <WaitingRoom roomCode={roomCode} />;
   }
+
   return (
     <div>
       <h1>Game Board</h1>
       <GameBoard />
     </div>
   );
+}
+
+export function getStaticProps(context) {
+  const roomCode = context.params.roomCode;
+
+  function verifyRoomCode(roomCode) {
+    //make sure that this room exists and this player is registered
+    // if not registered then spectator?
+    // axios(roomCode)
+
+    return true;
+  }
+
+  console.log(roomCode);
+  const isRoomCodeValid = verifyRoomCode(roomCode);
+
+  // Make some query to the server to see if the room exists or if the game is already started etc.
+  // should I allow spectator mode? Hmm...
+  // verifyRoomCode(roomCode);
+  return {
+    props: {
+      roomCode,
+      isRoomCodeValid,
+    },
+    revalidate: 1,
+  };
 }
 
 Game.propTypes = {};
