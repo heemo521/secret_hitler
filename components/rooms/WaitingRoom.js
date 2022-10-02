52ab875d8ec0f9c7f40a3f052e5d4e699dc56f48
Heemo Yang <heemo521@gmail.com>
2022-10-01 22:08:00 -0700

    Refactor Waiting Room with additional Waiting Players component

--- components/rooms/WaitingRoom.js
+++ components/rooms/WaitingRoom.js
@@ -1,32 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import WaitingPlayers from './WaitingPlayers';
+
+/*TODO: Copy button for the user, maybe create a custom message to invite game with a link */
+
+/* TODO: this button should only be visible to the game master  */
 
 function WaitingRoom({ onStartGame, gameData }) {
-  //extract players here after schema and etc.
   const { roomCode, host, players } = gameData;
   const numOfPlayers = players.length;
+  const gameStatus =
+    numOfPlayers > 5 ? 'Waiting for other players...' : 'Ready to start';
 
   return (
     <div>
-      {/* Copy button for the user, maybe create a custom message to invite game with a link */}
       <h1>Waiting Room</h1>
       <h2>Room Code: {roomCode}</h2>
-      {numOfPlayers > 5 ? (
-        <p>Waiting for other players...</p>
-      ) : (
-        <p>Ready to start</p>
-      )}
-
-      <ol>
-        {players.map((player) => {
-          return (
-            <li key={player.id}>
-              <p>{player.name}</p>
-            </li>
-          );
-        })}
-      </ol>
-      {/* TODO: this button should only be visible to the game master  */}
+      <p>{gameStatus}</p>
+      <WaitingPlayers host={host} players={players} />
       <button disabled={players.length < 5} onClick={onStartGame}>
         Start the game
       </button>
--- pages/lobby/index.js
+++ pages/lobby/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Head from 'next/head';
 import dbConnect from '../../utils/dbConnect';
 import Game from '../../models/game';
