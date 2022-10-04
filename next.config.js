b82814296e86532a23015cf4660cee6c3039e444
Heemo Yang <heemo521@gmail.com>
2022-10-03 23:43:51 -0700

    Still investigating2

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  outputFileTracing: false,
+  outputFileTracing: true,
   env: {
     MONGO_DB:
       'mongodb+srv://mymongo:mybongo@cluster0.d2cqwgg.mongodb.net/secret_hitler?retryWrites=true&w=majority',
